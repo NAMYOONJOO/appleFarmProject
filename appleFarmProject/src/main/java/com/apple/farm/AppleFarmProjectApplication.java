ffda83742689484971caa5c6d0bed69b7d97636b
NAMYOONJOO <bhd4445@naver.com>
2022-02-23 15:38:18 +0900

    Spring boot project commit

---
+++ appleFarmProject/src/main/java/com/apple/farm/AppleFarmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.apple.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleFarmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleFarmProjectApplication.class, args);
+	}
+
+}
